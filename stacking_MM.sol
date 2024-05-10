1ebca98b48ab0dd6dc257040df2d0b20df7375af
Anton K <105612467+drFaust24@users.noreply.github.com>
2024-05-10 03:18:37 +0300

    stake upd

---
+++ stacking_MM.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ProjectStakeContract is Ownable {
+
+    uint256 public rewardRate = 10;   // Staking Reward amount by default
+    uint256 public currentEpoch = 0;  // Epoch counter
+    uint256 currentTime;
+    uint256 DAY_IN_SECONDS = 60 * 60 * 24;
+    uint256 EPOCH_IN_SECONDS = DAY_IN_SECONDS * 7;  // 1 week
+    uint256 MAX_LOCK_DURATION = EPOCH_IN_SECONDS * 52;  // ~ 1 year
+    uint256 MAX_LOCK_AMOUNT = 10**7;  // 1% of total supply (or it should be 10 ** 18 * 10 ** 7 ?)
+    uint256 maxMulti = 3;
+    uint256 maxLockAmount = 10 * 7 * (10 * 18); 
+        
+    address TokenAddress;
+    address RToken;  
+    IERC20 StakeToken;
+
+    struct Stake {
+        uint256 amount;
+        uint256 startTime;
+        uint256 lockDuration;
+        uint256 reward;
+    }
+
+    mapping (address => Stake) public Stakers;
+
+    bool public emergencyStopTrigger = false;
+    bool public emergencyWithdrawTrigger = false;
+
+    event TokenUpdated(address owner, uint256 amount, uint256 epoch);
+    event TokenStaked(address owner, uint amount, uint nextEpoch, uint epoch_num, uint reward);
+    event TokenUnstaked(address owner, uint256 amount, uint256 RewardValue);
+    event Received(address, uint256);
+
+    // Init addresses on deploy
+    constructor (address _tokenAddress) {
+        TokenAddress = _tokenAddress; // NFT contract address [can't be changed later]
+        StakeToken = IERC20 (TokenAddress);
+    }
+    
+    // Staking 
+    function Staking(uint256 _amount, uint256 lock_duration) external {
+        require(emergencyStopTrigger == false, "Emergency stop active");
+        require(lock_duration <= MAX_LOCK_DURATION, "Epoch param more than max available");
+        require(lock_duration > 0 , "At least 1 epoch for staking");
+        require(_amount < maxLockAmount, "Amount more than max available");
+        require(_amount > 0, "Lock amount must be positive");
+        
+        currentTime = block.timestamp;
+
+        if (Stakers[msg.sender].amount != 0 ) {
+            
+            uint256 remaining_time = (Stakers[msg.sender].startTime + Stakers[msg.sender].lockDuration - currentTime);
+            if (remaining_time > (Stakers[msg.sender].lockDuration + EPOCH_IN_SECONDS)) {
+                revert("Invalid remaining time. Please try again.");
+            }
+            if (remaining_time <= EPOCH_IN_SECONDS) {
+                revert("Cannot deposit to stake nearing or past its end.");
+            }
+            currentEpoch = remaining_time;
+            uint256 _reward = _amount * currentEpoch * rewardRate / 100;
+            uint256 plusSum = Stakers[msg.sender].amount + _amount;
+            uint256 plusReward = Stakers[msg.sender].reward + _reward;
+            StakeToken.transferFrom(msg.sender, address(this), _amount);
+            Stakers[msg.sender] = Stake(plusSum, Stakers[msg.sender].startTime, lock_duration, plusReward);
+            emit TokenUpdated(msg.sender, _amount, _reward);
+
+        }
+
+        else {
+            
+            uint256 nextEpoch = get_next_epoch_start_time();
+            if (nextEpoch > 0) {
+                revert ("Invalid next epoch start time. Please try again.");
+            }
+            uint256 epoch_num = lock_duration;
+            uint256 _reward = _amount * epoch_num * rewardRate / 100;
+            StakeToken.transferFrom(msg.sender, address(this), _amount);
+            Stakers[msg.sender] = Stake(_amount, nextEpoch, epoch_num * EPOCH_IN_SECONDS, _reward);
+            emit TokenStaked(msg.sender, _amount, nextEpoch, epoch_num * EPOCH_IN_SECONDS, _reward);
+
+        }
+                
+    }
+
+    // New Epoch counter
+    function get_next_epoch_start_time() public view returns (uint256) {
+        uint256 current_time = block.timestamp;
+        uint256 days_since_unix_epoch = current_time / DAY_IN_SECONDS;
+        uint256 day_of_week = days_since_unix_epoch % 7;  // 0: Thursday, 1: Friday, ..., 6: Wednesday
+        uint256 seconds_from_thursday = day_of_week * DAY_IN_SECONDS + current_time % DAY_IN_SECONDS;
+        uint256 next_epoch_start_time = current_time + EPOCH_IN_SECONDS - seconds_from_thursday;
+        if (next_epoch_start_time <= current_time) {
+            return 0;
+        }
+        return next_epoch_start_time;
+    }
+
+
+    // Rewards are paying from this contract balance
+    function Unstake() external {
+
+        require(emergencyStopTrigger == false, "Emergency stop active");
+        if (emergencyWithdrawTrigger == true) {
+
+            StakeToken.transfer(msg.sender, Stakers[msg.sender].amount);
+            emit TokenUnstaked(msg.sender, Stakers[msg.sender].amount, 0);
+            delete Stakers[msg.sender];
+        }
+        else {
+            currentTime = block.timestamp;
+            if (currentTime <= (Stakers[msg.sender].startTime + Stakers[msg.sender].lockDuration)) {
+                revert("Cannot withdraw from stake that has not reached its end.");
+            }
+            uint256 amount_to_withdraw = Stakers[msg.sender].amount + Stakers[msg.sender].reward; 
+            if (amount_to_withdraw < (Stakers[msg.sender].amount * maxMulti)) {
+                revert("Cannot withdraw more than 300% of lock amount");
+            }
+            StakeToken.transfer(msg.sender, amount_to_withdraw);
+            
+            emit TokenUnstaked(msg.sender, Stakers[msg.sender].amount, Stakers[msg.sender].reward);
+            delete Stakers[msg.sender];
+        }
+        
+    }
+
+  
+// ----- Settings -----
+
+    // Change reward token address
+    function ChangeToken(address _new) external onlyOwner {
+        RToken = _new;
+    }
+
+    // Reward value settings
+    function SetRewardRate(uint256 _new) external onlyOwner {
+        rewardRate = _new;
+    }
+
+    function SetMaxAmount(uint256 _new) external onlyOwner {
+        maxLockAmount = _new;
+    }
+
+// ----- Emergency -----
+
+    function emergencyStop() external onlyOwner {
+        if (emergencyStopTrigger == false) { emergencyStopTrigger = true; }
+        else { emergencyStopTrigger = false; }
+    }
+
+    function emergencyWithdraw() external onlyOwner {
+        if (emergencyWithdrawTrigger == false) { emergencyWithdrawTrigger = true; }
+        else { emergencyWithdrawTrigger = false; }
+    }
+
+    // Emergency funds withdrawal 
+    function emergency() external onlyOwner {
+        IERC20 _RToken = IERC20(RToken);
+        _RToken.transfer(owner(), _RToken.balanceOf(address(this)));
+        payable(owner()).transfer(address(this).balance);
+    }
+
+// ----- View -----
+
+    function RewardTokenData() external view returns (address) {
+        return RToken;
+    }
+   
+    // View sum of total reward tokens on this contract balance
+    function RewardBalance() external view returns (uint256) {
+        IERC20 _RToken = IERC20(RToken);
+        return _RToken.balanceOf(address(this));
+    }
+
+    function Timestamp() external view returns (uint256) {
+        uint256 _timestamp = block.timestamp;
+        return _timestamp;
+    }
+    
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+
+}
